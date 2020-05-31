2cfdf49c449e9b63b3d988ff645f9ee2c011b499
unknown <jorammanoah@gmail.com>
2020-05-31 12:14:56 -0700

    Blog post moved into github

---
+++ blog/views.py
@@ -0,0 +1,9 @@
+from django.views import generic
+from .models import Post
+
+class PostList(generic.ListView):
+    queryset=Post.objects.filter(status=1).order_by('-created_on')
+    template_name='index.html'
+class PostDetail(generic.DeleteView):
+    model=Post
+    template_name='post_detail.html'
