2f653c091e0c849ceba09003d434982e9901eb03
unknown <jorammanoah@gmail.com>
2020-06-01 10:55:08 -0700

    Modified the whole repo

--- comment/settings.py
+++ comment/settings.py
@@ -28,7 +28,7 @@ SECRET_KEY = os.environ.get('DJANGO_SECRET_KEY', 'cg#p$g+j9tax!#a3cup@1$8obt2_+&
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['infinite-shore-14957.herokuapp.com','127.0.0.1',]
 
 
 # Application definition
